518ea12f00206430f30f57f2be40d87c179eba8f
rainy6 <ryo167prog@gmail.com>
2023-07-29 02:39:06 +0900

    update:19p 三角形の描画

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+use std::sync::Arc;
+
 use wasm_bindgen::prelude::*;
 use web_sys::console;
-
+use wasm_bindgen::JsCast;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -10,18 +12,59 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3]){
+    let [top, left, right] = points;
+    context.move_to(top.0, top.1);
+    context.begin_path();
+    context.line_to(left.0, left.1);
+    context.line_to(right.0, right.1);
+    context.line_to(top.0, top.1);
+    context.close_path();
+    context.stroke();
+}
+
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-    #[cfg(debug_assertions)]
+
     console_error_panic_hook::set_once();
 
+    // ここから
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
 
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    console::log_1(&JsValue::from_str("Hello World!"));
+
+    let canvas = document
+        .get_element_by_id("canvas")
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
+
+    console::log_1(&JsValue::from_str("Hello World2!"));
+
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    // context.move_to(300.0, 0.0); // 上の頂点
+    // context.begin_path();
+    // context.line_to(0.0, 600.0); // 左下の頂点
+    // context.line_to(600.0, 600.0); // 右下の頂点
+    // context.line_to(300.0, 0.0); // 上の頂点に戻る
+    // context.close_path();
+    // context.stroke();
+    // context.fill();
+
+
+    draw_triangle(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)]);
+    draw_triangle(&context, [(300.0, 0.0), (150.0, 300.0), (450.0, 300.0)]);
+    draw_triangle(&context, [(150.0, 300.0), (0.0, 600.0), (300.0, 600.0)]);
+    draw_triangle(&context, [(450.0, 300.0), (300.0, 600.0), (600.0, 600.0)]);
 
     Ok(())
 }
