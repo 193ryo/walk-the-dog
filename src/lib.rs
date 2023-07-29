88fb13841b38ede25a056acb2149bb0a11874f5c
rainy6 <ryo167prog@gmail.com>
2023-07-29 17:56:01 +0900

    update:p21 コメント追加

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,10 @@ use wasm_bindgen::JsCast;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+
+/*
+　三角形を描画する
+*/
 fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3]){
     let [top, left, right] = points;
     context.move_to(top.0, top.1);
@@ -23,13 +27,21 @@ fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64
     context.stroke();
 }
 
+/*
+　座標を指定し、三角形を描画する
+*/
 fn sierpinski(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 3], depth: u8) {
 
+    // 外枠の三角形を描画する
     draw_triangle(&context, points);
 
+    // 各座標の指定
     let [top, left, right] = points;
+
+    // 再帰描画回数
     let depth = depth - 1;
 
+    // 三角形を描画する
     if depth > 0 {
         let left_middle = midpoint(top, left);
         let right_middle = midpoint(top, right);
@@ -40,29 +52,28 @@ fn sierpinski(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64); 
     }
 }
 
+// 三角形の中心点を割出す
 fn midpoint(point_1: (f64, f64), point_2: (f64, f64)) -> (f64, f64) {
     ((point_1.0 + point_2.0) / 2.0, (point_1.1 + point_2.1) / 2.0)
 }
 
-// This is like the `main` function, except for JavaScript.
+/*
+　シェルピンスキーの三角形を描画する
+*/
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
 
+    // パニック時の内容を取得する
     console_error_panic_hook::set_once();
 
-    // ここから
+    // windowからcanvasを取得
     let window = web_sys::window().unwrap();
     let document = window.document().unwrap();
-
-    console::log_1(&JsValue::from_str("Hello World!"));
-
     let canvas = document
         .get_element_by_id("canvas")
         .unwrap()
         .dyn_into::<web_sys::HtmlCanvasElement>()
         .unwrap();
-
-    console::log_1(&JsValue::from_str("Hello World2!"));
 
     let context = canvas
         .get_context("2d")
@@ -71,6 +82,7 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
+    // 描画する
     sierpinski(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)], 2);
 
     Ok(())
